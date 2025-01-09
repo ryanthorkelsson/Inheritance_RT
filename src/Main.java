8f6b7062d030690f35bdcf45618d0e866b7f6b47
r.thorkelsson <ryanthorkelsson>
2025-01-09 11:36:19 -0700

    1/9 class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import Ex3_MusicFestival.Ex3_Main;
 import TwoD_Arrays.Ex1_Golf;
+import TwoD_Arrays.Ex2_TicTacToe;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Inheritance Unit");
@@ -8,7 +10,8 @@ public class Main {
 //        Ex1_Main.run();
 //        Ex2_Main.run();
 //        Ex3_Main.run();
-        Ex1_Golf.run();
+//        Ex1_Golf.run();
+        Ex2_TicTacToe.run();
 
     }
 
