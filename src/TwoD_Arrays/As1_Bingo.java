a69d5edf81881b1aafac1a3a26c7963a7f904396
r.thorkelsson <ryanthorkelsson>
2025-02-03 11:39:28 -0700

    2/3 class

--- src/TwoD_Arrays/As1_Bingo.java
+++ src/TwoD_Arrays/As1_Bingo.java
@@ -17,12 +17,82 @@ public class As1_Bingo {
 
 
             for (int row = 0; row < bingoCard.length; row++) {
+                System.out.print(bingo[row]);
                 for (int col = 0; col < bingoCard.length; col++) {
-                    System.out.print(bingo[row] + "  " + bingoCard[row]);
+                    System.out.print("  " + bingoCard[row][col]);
                 }
                 System.out.println();
             }
-            
+
+            System.out.println();
+            System.out.println("Press Enter to get a number");
+
+            while (true){
+
+                Library.input.nextLine();
+                int drawNum = Library.myRandom(1,75);
+                System.out.println("The number drawn is: " + drawNum);
+                System.out.println();
+                System.out.println("Press ENTER to get another number");
+
+
+                boolean foundNum = false;
+
+                for (int row = 0; row <bingoCard.length ; row++) {
+                    for (int col = 0; col <bingoCard.length ; col++) {
+                        if(drawNum == bingoCard[row][col] && !(bingoCard[row][col] < 0)){
+                            bingoCard[row][col] *= -1;
+                            foundNum = true;
+                        }
+                    }
+                }//for
+
+                if(!foundNum){
+                    System.out.println();
+                    System.out.println("Not found");
+                    System.out.println();
+                }
+
+                for (int row = 0; row < bingoCard.length; row++) {
+                    System.out.print(bingo[row]);
+                    for (int col = 0; col < bingoCard.length; col++) {
+                        System.out.print("  " + bingoCard[row][col]);
+                    }
+                    System.out.println();
+                }//for
+
+                boolean win = false;
+                for (int i = 0; i < bingoCard.length; i++) {
+                    if(checkCol(bingoCard,i ) != -1){
+                        System.out.println("BINGO! You Win!");
+                        win = true;
+                        break;
+                    }
+                }
+
+                for (int i = 0; i < bingoCard.length; i++) {
+                    if(checkRow(bingoCard[i]) != -1){
+                        System.out.println("BINGO! You Win!");
+                        win = true;
+                        break;
+                    }
+                }
+                if(checkDiag(bingoCard) != -1){
+                    System.out.println("BINGO! You Win!");
+                    win = true;
+                    break;
+                }
+                if(win){
+                    break;
+                }
+
+            }//while
+
+
+
+
+
+
         }//run
 
         public static int[] generateArray(int length, int min, int max){
@@ -34,5 +104,44 @@ public class As1_Bingo {
 
             return newArr;
         }//generateArray
+
+        public static int checkRow(int[] row){
+            int counter =0;
+            for (int i = 0; i < row.length; i++) {
+                if(row[i] < 0){
+                    counter ++;
+                }
+            }
+            if(counter >4){
+                return 1;
+            }
+            return -1;
+        }
+
+        public static int checkCol(int[][] grid, int colNum){
+            int counter =0;
+            for (int i = 0; i < grid.length; i++) {
+                if(grid[i][colNum] <0){
+                    counter ++;
+                }
+            }
+            if(counter >4){
+                return 1;
+            }
+            return -1;
+        }
+
+        public static int checkDiag (int[][] arr){
+            //Diagonal Down Right
+            if((arr[0][0] < 0)  && (arr[1][1] <0)  && (arr[2][2] <0) && (arr[3][3] <0) && (arr[4][4] <0)){
+                return 1;
+            }
+
+            //diagonal Up Right
+            if((arr[0][4] <0) && (arr[1][3] <0) && (arr[2][2] <0) && (arr[3][1] <0) && (arr[4][0] <0)){
+                return 1;
+            }
+            return -1;
+        }
 
 }
