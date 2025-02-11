8a6ecf7620dbc7d049da66b26032be6673c2f940
r.thorkelsson <ryanthorkelsson>
2025-02-11 11:39:07 -0700

    2/7 class

--- src/TwoD_Arrays/As5_Vigenere.java
+++ src/TwoD_Arrays/As5_Vigenere.java
@@ -5,8 +5,24 @@ public class As5_Vigenere {
         char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
                 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
         char[][] vigenere = createVSquare(alphabet);
+
+
+        System.out.println("Please enter a keyword for decrypting");
+        String key = Library.input.nextLine();
+
+        System.out.println("Please enter an encrypted message");
+        String encrypted = Library.input.nextLine();
+        Library.input.nextLine();
+
+
         printSquare(vigenere);
 
+        System.out.println("The Decrypted message is:");
+        System.out.println(decrypt(encrypted,vigenere,key));
+
+//        System.out.println(decrypt("OGOGTSEYNTVCKA",vigenere));
+//        System.out.println(decrypt("XWBGIEGGJILJHJOVKARNKKCAADCFEAQU",vigenere));
+//        System.out.println(decrypt("LJOGCGWDEIWKGBNWJOADKQARFWNZBW",vigenere));
 
     }//end run
 
@@ -52,17 +68,27 @@ public class As5_Vigenere {
     }//getChar
 
 
-    public static char[] decrypt(String cipherText, char[][] arr) {
-        char[] keyWord = {'S', 'C', 'O', 'N', 'A'};
+    public static char[] decrypt(String cipherText, char[][] arr,String key) {
+
+        char[] keyWord = key.toCharArray();
         char[] textArr = cipherText.toCharArray();
         char[] n = new char[textArr.length];
+        int keyIndex =0;
 
         for (int i = 0; i < textArr.length; i++) {
-            for (int j = 0; j < keyWord.length; j++) {
-                int index1 = linearSearch(arr[0], textArr[i]);
-                int index2 = linearSearch(arr[][0])
+            for (int j = 0; j < arr.length; j++) {
+                int Index1 = linearSearch(arr[0],keyWord[keyIndex]);
+                int Index2 = linearSearch(arr[Index1],textArr[i]);
+
+                n[i] = arr[0][Index2];
+
+                keyIndex ++;
+                if(keyIndex >4){
+                    keyIndex =0;
+                }
             }
         }
+
         return n;
     }
 
